c45d55ce84e63b91307b3a3b7ff5aefd620afd67
masamonoke <m.rudakov@yahoo.com>
2023-10-21 12:05:41 +0300

    init

---
+++ init.vim
@@ -0,0 +1,176 @@
+if $TERM_PROGRAM != "Apple_Terminal"
+    set termguicolors
+endif
+syntax on
+set ts=4 sw=4
+set nostartofline
+set number
+set mouse=a
+set noswapfile
+set nowrap
+set hlsearch
+set clipboard+=unnamedplus
+
+call plug#begin()
+Plug 'preservim/nerdtree'
+Plug 'windwp/nvim-autopairs'
+Plug 'tpope/vim-commentary'
+Plug 'lfv89/vim-interestingwords'
+Plug 'tpope/vim-fugitive'
+Plug 'neoclide/coc.nvim', {'branch': 'master', 'do': 'yarn install --frozen-lockfile'}
+Plug 'nvim-tree/nvim-web-devicons'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'karb94/neoscroll.nvim'
+Plug 'itchyny/vim-gitbranch'
+Plug 'voldikss/vim-floaterm'
+Plug 'lukas-reineke/indent-blankline.nvim'
+Plug 'ARM9/arm-syntax-vim'
+
+Plug 'sainnhe/gruvbox-material'
+Plug 'ellisonleao/gruvbox.nvim'
+Plug 'luisiacc/gruvbox-baby', {'branch': 'main'}
+Plug 'neovim/nvim-lspconfig'
+Plug 'xiyaowong/nvim-transparent'
+Plug 'justinmk/vim-sneak'
+Plug 'pangloss/vim-javascript'
+Plug 'leafgarland/typescript-vim'
+Plug 'peitalin/vim-jsx-typescript'
+Plug 'maxmellon/vim-jsx-pretty'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'make' }
+Plug 'samodostal/image.nvim'
+Plug 'yamatsum/nvim-cursorline'
+Plug 'p00f/clangd_extensions.nvim'
+Plug 'godlygeek/tabular'
+Plug 'preservim/vim-markdown'
+Plug 'shime/vim-livedown'
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+Plug 'p00f/nvim-ts-rainbow'
+Plug 'universal-ctags/ctags'
+Plug 'untitled-ai/jupyter_ascending.vim'
+Plug 'jalvesaq/Nvim-R'
+Plug 'ryanoasis/vim-devicons'
+Plug 'folke/todo-comments.nvim'
+Plug 'ntpeters/vim-better-whitespace'
+Plug 'Vigemus/iron.nvim'
+Plug 'fs111/pydoc.vim'
+call plug#end()
+
+"color deus
+"color gruvbox-material
+color gruvbox
+"color gruvbox-baby
+
+" let g:rainbow#pairs = [['(', ')'], ['[', ']'], ['{', '}']]
+" autocmd VimEnter * RainbowParentheses
+" let g:rainbow#max_level = 20
+" let g:rainbow#blacklist = [188, 231, 215, 220, 230, 180, 221, 226, "#ebdbb2", "#ffa61a"]
+
+let g:floaterm_keymap_new    = '<F7>'
+let g:floaterm_keymap_toggle = '<F12>'
+let g:floaterm_keymap_next = '<F8>'
+let g:floaterm_keymap_kill = '<F9>'
+let g:floaterm_wintype = 'split'
+let g:floaterm_height = 20
+hi Floaterm guibg=black
+
+nmap <leader>1 <Plug>BuffetSwitch(1)
+nmap <leader>2 <Plug>BuffetSwitch(2)
+nmap <leader>3 <Plug>BuffetSwitch(3)
+nmap <leader>4 <Plug>BuffetSwitch(4)
+nmap <leader>5 <Plug>BuffetSwitch(5)
+nmap <leader>6 <Plug>BuffetSwitch(6)
+nmap <leader>7 <Plug>BuffetSwitch(7)
+nmap <leader>8 <Plug>BuffetSwitch(8)
+nmap <leader>9 <Plug>BuffetSwitch(9)
+nmap <leader>0 <Plug>BuffetSwitch(10)
+noremap <Tab> :bn<CR>
+noremap <S-Tab> :bp<CR>
+noremap <C-t> :tabnew split<CR>
+
+autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
+nmap <F6> :NERDTreeToggle<CR>
+nnoremap <F4> :bp \| sp \| bn \| bd<CR>
+let g:NERDTreeWinSize=50
+let NERDTreeShowHidden=1
+
+set guicursor=i:block
+
+
+let g:buffet_powerline_separators = 1
+let g:buffet_tab_icon = "\uf00a"
+let g:buffet_left_trunc_icon = "\uf0a8"
+let g:buffet_right_trunc_icon = "\uf0a9"
+let g:buffet_show_index = 1
+
+highlight CocErrorFloat ctermfg=204 guifg=#ffffff
+
+au BufNewFile,BufRead *.s,*.S set filetype=arm " arm = armv6/7
+
+let g:indentLine_setColors = 0
+
+nmap gm :LivedownToggle<CR>
+
+lua require('config')
+
+nmap <space><space>x <Plug>JupyterExecute
+nmap <space><space>X <Plug>JupyterExecuteAll
+nmap <space><space>r <Plug>JupyterRestart
+
+autocmd TextChanged,TextChangedI <buffer> silent write
+
+let g:indentLine_char_list = ['|', '¦', '┆', '┊']
+
+"coc
+inoremap <expr> <TAB> pumvisible() ? "\<C-y>" : "\<CR>"
+
+lua << EOF
+require('telescope').load_extension('fzf')
+EOF
+
+nnoremap <F1> :Telescope buffers <CR>
+nnoremap <F2> :Telescope find_files<CR>
+nnoremap <F3> :Telescope live_grep<CR>
+
+lua << EOF
+require("nvim-autopairs").setup {}
+EOF
+
+autocmd BufRead *.rs :setlocal tags=./rusty-tags.vi;/
+
+autocmd FileType html setlocal sw=2 ts=2
+autocmd FileType vue setlocal sw=2 ts=2
+autocmd FileType r setlocal sw=1 ts=1 expandtab
+autocmd FileType xml setlocal sw=2 ts=2
+
+let g:airline_powerline_fonts = 1
+let g:airline_theme = "deus"
+"let g:airline#extensions#tabline#enabled = 1
+
+lua << EOF
+  require("todo-comments").setup {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+EOF
+
+" Check if NERDTree is open or active
+function! IsNERDTreeOpen()
+  return exists("t:NERDTreeBufName") && (bufwinnr(t:NERDTreeBufName) != -1)
+endfunction
+
+set keywordprg=:call\ CocActionAsync('doHover')
+augroup VimHelp
+  autocmd!
+  autocmd Filetype vim,help setlocal keywordprg=:help
+augroup END
+
+set langmap=ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz
+
+hi @define.c guifg=coral3
+hi @constant.c guifg=IndianRed1
+
+let g:pydoc_open_cmd = 'tabnew'
---
+++ lua/config.lua
@@ -0,0 +1,247 @@
+-- Require and call setup function somewhere in your init.lua
+require('image').setup {
+  render = {
+    min_padding = 0,
+    show_label = true,
+    use_dither = true,
+    foreground_color = false,
+    background_color = false
+  },
+  events = {
+    update_on_nvim_resize = true,
+  },
+}
+
+require('nvim-cursorline').setup {
+  cursorline = {
+    enable = true,
+    timeout = 1000,
+    number = false,
+  },
+  cursorword = {
+    enable = true,
+    min_length = 3,
+    hl = { underline = true },
+  }
+}
+
+require("clangd_extensions").setup {
+    server = {
+        -- options to pass to nvim-lspconfig
+        -- i.e. the arguments to require("lspconfig").clangd.setup({})
+    },
+    extensions = {
+        -- defaults:
+        -- Automatically set inlay hints (type hints)
+        autoSetHints = true,
+        -- These apply to the default ClangdSetInlayHints command
+        inlay_hints = {
+            -- Only show inlay hints for the current line
+            only_current_line = false,
+            -- Event which triggers a refersh of the inlay hints.
+            -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+            -- not that this may cause  higher CPU usage.
+            -- This option is only respected when only_current_line and
+            -- autoSetHints both are true.
+            only_current_line_autocmd = "CursorHold",
+            -- whether to show parameter hints with the inlay hints or not
+            show_parameter_hints = true,
+            -- prefix for parameter hints
+            parameter_hints_prefix = "<- ",
+            -- prefix for all the other hints (type, chaining)
+            other_hints_prefix = "=> ",
+            -- whether to align to the length of the longest line in the file
+            max_len_align = false,
+            -- padding from the left if max_len_align is true
+            max_len_align_padding = 1,
+            -- whether to align to the extreme right or not
+            right_align = false,
+            -- padding from the right if right_align is true
+            right_align_padding = 7,
+            -- The color of the hints
+            highlight = "Comment",
+            -- The highlight group priority for extmark
+            priority = 100,
+        },
+        ast = {
+            -- These are unicode, should be available in any font
+            role_icons = {
+                 type = "🄣",
+                 declaration = "🄓",
+                 expression = "🄔",
+                 statement = ";",
+                 specifier = "🄢",
+                 ["template argument"] = "🆃",
+            },
+            kind_icons = {
+                Compound = "🄲",
+                Recovery = "🅁",
+                TranslationUnit = "🅄",
+                PackExpansion = "🄿",
+                TemplateTypeParm = "🅃",
+                TemplateTemplateParm = "🅃",
+                TemplateParamObject = "🅃",
+            },
+            --[[ These require codicons (https://github.com/microsoft/vscode-codicons)
+            role_icons = {
+                type = "",
+                declaration = "",
+                expression = "",
+                specifier = "",
+                statement = "",
+                ["template argument"] = "",
+            },
+
+            kind_icons = {
+                Compound = "",
+                Recovery = "",
+                TranslationUnit = "",
+                PackExpansion = "",
+                TemplateTypeParm = "",
+                TemplateTemplateParm = "",
+                TemplateParamObject = "",
+            }, ]]
+
+            highlights = {
+                detail = "Comment",
+            },
+        },
+        memory_usage = {
+            border = "none",
+        },
+        symbol_info = {
+            border = "none",
+        },
+    },
+}
+
+require('neoscroll').setup({
+    -- All these keys will be mapped to their corresponding default scrolling animation
+    mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+                '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+    hide_cursor = true,          -- Hide cursor while scrolling
+    stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+    respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+    cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+    easing_function = nil,       -- Default easing function
+    pre_hook = nil,              -- Function to run before the scrolling animation starts
+    post_hook = nil,             -- Function to run after the scrolling animation ends
+    performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+})
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the four listed parsers should always be installed)
+  ensure_installed = { "lua", "vim" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = false,
+
+  -- List of parsers to ignore installing (for "all")
+  --ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    
+    -- disable = function(lang, buf)
+    --     local max_filesize = 100 * 1024 -- 100 KB
+    --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    --     if ok and stats and stats.size > max_filesize then
+    --         return true
+    --     end
+    -- end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+require("nvim-treesitter.configs").setup {
+  highlight = {
+      -- ...
+  },
+  -- ...
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  }
+}
+
+require("indent_blankline").setup {
+    -- for example, context is off by default, use this to turn it on
+    show_current_context = true,
+    show_current_context_start = true,
+
+}
+
+local iron = require("iron.core")
+
+iron.setup {
+  config = {
+    -- Whether a repl should be discarded or not
+    scratch_repl = true,
+    -- Your repl definitions come here
+    repl_definition = {
+      sh = {
+        -- Can be a table or a function that
+        -- returns a table (see below)
+        command = {"zsh"}
+      }
+    },
+    -- How the repl window will be displayed
+    -- See below for more information
+    --repl_open_cmd = require('iron.view').split.vertical.botright(50),
+	repl_open_cmd = require('iron.view').center("90%", 40),
+  },
+  -- Iron doesn't set keymaps by default anymore.
+  -- You can set them here or manually add keymaps to the functions in iron.core
+  keymaps = {
+    send_motion = "<space>sc",
+    visual_send = "<space>sc",
+    send_file = "<space>sf",
+    send_line = "<space>sl",
+    send_until_cursor = "<space>su",
+    send_mark = "<space>sm",
+    mark_motion = "<space>mc",
+    mark_visual = "<space>mc",
+    remove_mark = "<space>md",
+    cr = "<space>s<cr>",
+    interrupt = "<space>s<space>",
+    exit = "<space>sq",
+    clear = "<space>cl",
+  },
+  -- If the highlight is on, you can change how it looks
+  -- For the available options, check nvim_set_hl
+  highlight = {
+    italic = true
+  },
+  ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+}
+
+-- iron also has a list of commands, see :h iron-commands for all available commands
+vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+
+
